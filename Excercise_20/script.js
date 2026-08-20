dae489c033f50f17d121c8f40b2db78ff9f5449e
alipnm <alipnm110@gmail.com>
2026-08-21 00:51:12 +0330

    Excercise_20 Done
    Yipeeee...
    Amirreza?? Baaaaa...

--- Excercise_20/script.js
+++ Excercise_20/script.js
@@ -1,3 +1,33 @@
-// bichare shodim sare in :) Amirreza?? baaa...
-let guesses = 7;
-// badbakhti haieman inja tamam shod. -> data structure, term 3 university, time complexity -> kheili tarsnak samtesh narid lotfan -> tarkibi az dm va ap
+let timeComplexity = 7;
+let secretNumber = Math.round (Math.random () * 100);
+const buttonElement = document.getElementsByTagName ('button')[0];
+const numberElement = document.getElementById ('number');
+const rootElement = document.getElementById ('root');
+
+console.log (secretNumber);
+
+buttonElement.addEventListener ('click', () => {
+  if (timeComplexity > 0) {
+    timeComplexity--;
+    let number = Number (numberElement.value);
+    if (number < secretNumber) {
+      rootElement.innerHTML += `<h1>Your guess: ${number}</h1>
+      gusses left: ${timeComplexity}
+        <h1>Your guess is lower than the secret number!</h1>
+        `;
+    } else if (number > secretNumber) {
+      rootElement.innerHTML += `<h1>Your guess: ${number}</h1>
+      gusses left: ${timeComplexity}
+        <h1>Your guess is higher than the secret number!</h1>
+        `;
+    } else {
+      buttonElement.disabled = true;
+      rootElement.innerHTML += '<h1>Your number was correct!!</h1>';
+    }
+  } else {
+    buttonElement.disabled = true;
+    rootElement.innerHTML = `<h1>Your guesses are over!
+    The number was ${secretNumber}</h1>
+    `;
+  }
+});
