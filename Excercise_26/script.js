e5e6fa93a84adfe6ba2c788d551862d0b74c547e
alipnm <alipnm110@gmail.com>
2026-09-19 22:12:31 +0330

    Excercise_26 Done
    Amirreza??? Baa...

--- Excercise_26/script.js
+++ Excercise_26/script.js
@@ -1,2 +1,37 @@
 const arrayContainerElement = document.getElementById("array");
+const addNameBtnElement = document.getElementById("add");
+const deleteNameBtnElement = document.getElementById("delete");
+const nameInputElement = document.getElementById("name");
 let names = ["ali", "sara", "sina", "reza", "hana", "mostafa"];
+
+let addName = () => {
+  let name = nameInputElement.value.toLowerCase().trim();
+  names.push(name);
+  nameInputElement.value = "";
+
+  showArray();
+};
+let deleteName = () => {
+  let name = nameInputElement.value;
+  if (names.find((n) => n.toLowerCase() === name) !== undefined) {
+    names.splice(names.indexOf(name), 1);
+  }
+
+  nameInputElement.value = "";
+  showArray();
+};
+let showArray = () => {
+  let result = [];
+  names.forEach((n) => {
+    result.push(`<h3>${n}</h3>`);
+  });
+
+  arrayContainerElement.innerHTML = result.join("");
+};
+
+addNameBtnElement.addEventListener("click", addName);
+deleteNameBtnElement.addEventListener("click", deleteName);
+
+window.onload = () => {
+  showArray();
+};
